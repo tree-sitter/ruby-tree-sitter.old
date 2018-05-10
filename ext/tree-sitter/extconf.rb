4d2d23ccd82ba14f3cc8386c135e7e1bd33bb7b5
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2018-05-10 01:09:27 -0400

    Dynamically set proper extension

--- ext/tree-sitter/extconf.rb
+++ ext/tree-sitter/extconf.rb
@@ -11,6 +11,21 @@ SITEARCH = RbConfig::CONFIG['sitearch']
 LIBDIR      = RbConfig::CONFIG['libdir']
 INCLUDEDIR  = RbConfig::CONFIG['includedir']
 
+OS         = case RbConfig::CONFIG['host_os']
+             when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+               :windows
+             when /darwin|mac os/
+               :macos
+             when /linux/
+               :linux
+             when /solaris|bsd/
+               :unix
+             else
+               raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
+             end
+
+SHARED_EXT = OS == :macos ? 'bundle' : 'so'
+
 ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 THIS_DIR = File.dirname(__FILE__)
 OUT_DIR = File.join(THIS_DIR, 'out')
@@ -19,7 +34,7 @@ TREE_SITTER_DIR = File.expand_path(File.join(File.dirname(__FILE__), 'tree-sitte
 TREE_SITTER_INCLUDE_DIR = File.join(TREE_SITTER_DIR, 'include')
 TREE_SITTER_SRC_DIR = File.join(TREE_SITTER_DIR, 'src')
 
-BUNDLE_PATH = File.join(ROOT, 'lib', 'tree-sitter', 'treesitter.bundle')
+BUNDLE_PATH = File.join(ROOT, 'lib', 'tree-sitter', "treesitter.#{SHARED_EXT}")
 
 HEADER_DIRS = [INCLUDEDIR, TREE_SITTER_INCLUDE_DIR]
 LIB_DIRS = [LIBDIR, OUT_DIR]
