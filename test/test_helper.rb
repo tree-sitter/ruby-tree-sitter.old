a8229118333f16010d9eee39cd043d761768e009
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2018-05-10 19:45:32 -0400

    Update test helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,8 @@ require 'tree-sitter'
 
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'minitest/focus'
+require 'pry'
 
 def fixtures_dir
   File.join(File.dirname(__FILE__), 'tree-sitter', 'fixtures')
